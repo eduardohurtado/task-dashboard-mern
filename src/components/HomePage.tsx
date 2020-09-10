1bf4a0f0a6d35a3305e3aaba82710f16d9a0dd17
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2020-09-09 22:25:44 -0500

    Beta Version 1.2.0 - Added responsive navbar, improved visuals.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,11 +4,14 @@ import React, { Component } from "react";
 import "../sass/homePage.scss";
 
 //Images
-import logoHtml from "../img/html-5.png";
-import logoScss from "../img/scss.png";
-import logoReact from "../img/react.png";
-import logoRedux from "../img/redux.png";
-import logoTs from "../img/typescript.png";
+import LogoHtml from "../img/html-5.png";
+import LogoScss from "../img/scss.png";
+import LogoReact from "../img/react.png";
+import LogoRedux from "../img/redux.png";
+import LogoTs from "../img/typescript.png";
+import LogoMongoDb from "../img/mongodb.png";
+import LogoExpress from "../img/express.png";
+import LogoNodeJs from "../img/nodejs.png";
 
 export default class HomePage extends Component {
   render() {
@@ -19,13 +22,36 @@ export default class HomePage extends Component {
           Welcome to my application, this app is useful to create dialy tasks
           that can be deleted, and marked as complete, please enjoy.
         </p>
-        <h2>Side technologies + MERN stack:</h2>
+        <h2>Main technologies:</h2>
 
-        <img src={logoHtml} alt="Html5" height="110px" />
-        <img src={logoTs} alt="Typescript" height="110px" />
-        <img src={logoScss} alt="SCSS" height="110px" />
-        <img src={logoReact} alt="React" height="110px" />
-        <img src={logoRedux} alt="Redux" height="110px" />
+        <img src={LogoHtml} alt="Html5" height="110px" />
+        <img src={LogoTs} alt="Typescript" height="110px" />
+        <img src={LogoScss} alt="SCSS" height="110px" />
+        <img src={LogoRedux} alt="Redux" height="110px" />
+
+        <h2>MERN stack:</h2>
+        <h3 className="grayIncoming">
+          *Gray icons means that feature is in current develop*
+        </h3>
+        <img
+          src={LogoMongoDb}
+          alt="MongoDb"
+          height="110px"
+          className="grayIncoming"
+        />
+        <img
+          src={LogoExpress}
+          alt="Express"
+          height="110px"
+          className="grayIncoming"
+        />
+        <img src={LogoReact} alt="React" height="110px" />
+        <img
+          src={LogoNodeJs}
+          alt="NodeJs"
+          height="110px"
+          className="grayIncoming"
+        />
       </div>
     );
   }
